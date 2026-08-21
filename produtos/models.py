a8d440361a859c8b77231035b5dc5dd0cf41a01b
Ana Clara Queiroz Bezerra <20231094010044@ifrn.local>
2026-08-21 14:21:52 -0300

    models

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class produto (models.Model):
+    nome=models.CharField(max_length=50)
+    idade=models.IntegerField()
+    email=models.EmailField(max_length=254)
+    
+    def  __str__(self):
+        return self.name
+    
--- produtos/views.py
+++ produtos/views.py
@@ -1,47 +1,13 @@
 from django.shortcuts import render
 
-def retunProdutos():
-    produtos = [
-        {'id': 1, 
-         'nome': 'Fone sem fio',
-         'lote': '001',
-         'categoria': 'Eletrônicos',
-         'quantidade': 3
-         },
-        {'id': 2, 
-         'nome': 'Vestido mid',
-         'lote': '002',
-         'categoria': 'Vestiário',
-         'quantidade': 6
-         },
-        {'id': 3, 
-         'nome': 'Base Dior',
-         'lote': '003',
-         'categoria': 'Cosméticos',
-         'quantidade': 2
-         },
-        {'id': 4, 
-         'nome': 'Máquina de lavar roupa',
-         'lote': '004',
-         'categoria': 'Eletrodoméstico',
-         'quantidade': 8
-         },
-        {'id': 5, 
-         'nome': 'Cif multiuso',
-         'lote': '005',
-         'categoria': 'Limpeza',
-         'quantidade': 19
-         }
-    ]
-    return produtos
+from . import models
 
 # Create your views here.
 def listagem(request):
-    context = {'produtos': retunProdutos()}
-    return render(request, 'produtos/listagem.html', context)
+    alunos = models.aluno.objects.all()
+    return render(request, 'produtos/listagem.html', {'alunos': alunos})
 
 def detalhes(request, id):
-    for item in retunProdutos():
-        if item['id'] == id:
-            context = item
-    return render(request, 'produtos/detalhes.html', context)
+    aluno = models.aluno.objects.get(id=id)
+    return render(request, 'produtos/listagem.html', {'alunos': alunos})
+    
